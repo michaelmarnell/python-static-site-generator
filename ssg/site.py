1b9de5f6bd8d30e53c9f9db6ea850eabd06cb97e
michaelmarnell <marnell@berkeley.edu>
2023-04-24 12:14:46 -0700

    status

--- ssg/site.py
+++ ssg/site.py
@@ -8,12 +8,12 @@ class Site:
     
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
-        directory.mkdir(parents = True, exists_ok = True)
+        directory.mkdir(parents = True, exist_ok = True)
     
     def build(self):
-        self.mkdir(parents = True, exists_ok = True)
+        self.dest.mkdir(parents = True, exist_ok = True)
         for path in self.source.rglob("*"):
-            if path.isdir():
+            if path.isdir() == True:
                 self.create_dir(path)
     
 
